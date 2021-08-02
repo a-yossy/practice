908edf5a7056ead1f82bd9f2c7d58981ad796061
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-08-02 22:27:10 +0900

    fix: CountAppコンポーネントとPluButtonコンポーネントを作成

--- count-app/src/index.js
+++ count-app/src/index.js
@@ -1,86 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-          className="square"
-          onClick={() => this.props.onClick()}  
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+class PlusButton extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.handleClick}>+</button>
+    )
   }
-  
-class Count extends React.Component {
-constructor(props) {
+}
+
+class CountApp extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-        value: ['+', '-'],
-        count: 0, 
+      count: 0,
     };
-}
+  }
 
-handleClick(i) {
-    let count = this.state.count;
-    if (i) {
-    count -= 1;
-    } else {
-    count += 1;
-    }
+  handlePlusButtonClick() {
     this.setState({
-    count: count,
+      count: this.state.count + 1,
     })
-}
+  }
 
-renderSquare(i) {
+  render() {
     return (
-    <Square 
-        value={this.state.value[i]}
-        onClick={() => this.handleClick(i)}
-    />
-    );
-}
-
-render() {
-    const status = 'カウント値： ' + this.state.count;
-
-    return (
-    <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        </div>
-    </div>
-    );
-}
-}
-
-class Game extends React.Component {
-render() {
-    return (
-    <div className="game">
-        <div className="game-board">
-        <Count />
-        </div>
-        <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-        </div>
-    </div>
-    );
-}
+      <>
+        <h1>Count App</h1>
+        <div>{this.state.count}</div>
+        <PlusButton
+          handleClick={() => this.handlePlusButtonClick()}
+        />
+      </>
+    )
+  }
 }
 
 // ========================================
 
 ReactDOM.render(
-<Game />,
+  <CountApp />,
 document.getElementById('root')
 );
-  
-  
