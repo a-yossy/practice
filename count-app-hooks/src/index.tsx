bc8d7d178b9b61fb376388449eade41eb9a1a93d
atsuya02 <atsuya0930@yahoo.co.jp>
2021-08-09 00:57:55 +0900

    feat: ボタンコンポーネント作成

--- count-app-hooks/src/index.tsx
+++ count-app-hooks/src/index.tsx
@@ -2,15 +2,40 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 const { useState } = React;
 
+type ButtonProps = {
+  buttonText: string,
+  onClick: () => void,
+}
+
+const Button: React.FC<ButtonProps> = ({ buttonText, onClick }) => {
+  return (
+    <button onClick={() => onClick()}>{buttonText}</button>
+  )
+}
 
 const CountApp: React.FC = () => {
   const [count, setCount] = useState(0);
+
+  const plus = () => {
+    setCount(count + 1)
+  }
+
+  const minus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <>
       <h1>Count App</h1>
       <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <Button
+        buttonText="+"
+        onClick={plus}
+      />
+      <Button
+        buttonText="-"
+        onClick={minus}
+      />
     </>
   )
 }
