d8ad028bdd24ad2c87941284cf7a4ab7427cfd91
a-yossy <atsuya0930@yahoo.co.jp>
2023-07-23 13:24:26 +0900

    finish

--- javascript/the-super-tiny-compiler/index.js
+++ javascript/the-super-tiny-compiler/index.js
@@ -135,3 +135,148 @@ function parser(tokens) {
 
   return ast;
 }
+
+
+function traverser(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type];
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      case 'Program':
+        traverseArray(node.body, node);
+        break;
+      case 'CallExpression':
+        traverseArray(node.params, node);
+        break;
+      case 'NumberLiteral':
+      case 'StringLiteral':
+        break;
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+
+  traverseNode(ast, null);
+}
+
+
+function transformer(ast) {
+  let newAst = {
+    type: 'Program',
+    body: [],
+  };
+
+  ast._context = newAst.body;
+
+  traverser(ast, {
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'NumberLiteral',
+          value: node.value,
+        });
+      }
+    },
+
+    StringLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'StringLiteral',
+          value: node.value,
+        });
+      },
+    },
+
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name,
+          },
+          arguments: [],
+        };
+
+        node._context = expression.arguments;
+
+        if (parent.type !== 'CallExpression') {
+          expression = {
+            type: 'ExpressionStatement',
+            expression,
+          };
+        }
+
+        parent._context.push(expression);
+      },
+    }
+  });
+
+  return newAst;
+}
+
+
+function codeGenerator(node) {
+  switch (node.type) {
+    case 'Program':
+      return node.body.map(codeGenerator).join('\n');
+
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression) + ';'
+      );
+
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator).join(', ') +
+        ')'
+      );
+
+    case 'Identifier':
+      return node.name;
+
+    case 'NumberLiteral':
+      return node.value;
+
+    case 'StringLiteral':
+      return '"' + node.value + '"';
+
+    default:
+      throw new TypeError(node.type);
+  }
+}
+
+
+function compiler(input) {
+  let tokens = tokenizer(input);
+  let ast = parser(tokens);
+  let newAst = transformer(ast);
+  let output = codeGenerator(newAst);
+
+  return output;
+}
+
+
+module.exports = {
+  tokenizer,
+  parser,
+  traverser,
+  transformer,
+  codeGenerator,
+  compiler,
+};
