0da4a851e90d82c955c7b63dde8dd0fb0c7ebb27
a-yossy <atsuya0930@yahoo.co.jp>
2023-12-03 16:46:53 +0900

    ssg

--- react/ssg/fixtures/ssg-app/src/pages/index.js
+++ react/ssg/fixtures/ssg-app/src/pages/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// TODO
+const ToyReact = require('toy-react');
+const ToyReactDOM = require('toy-react-dom');
+
+require('../assets/styles/global.css');
+
+const { App } = require('../components/App');
+
+ToyReactDOM.hydrate(<App />, document.getElementById('root'));
