ba594576e2a48a3a67e492e2c454b1eed182765e
a-yossy <atsuya0930@yahoo.co.jp>
2024-07-21 23:15:49 +0900

    fix

---
+++ rust/hello/target/debug/deps/hello-338f38284a7aca52.d
@@ -0,0 +1,5 @@
+/Users/atsuya/practice/rust/hello/target/debug/deps/hello-338f38284a7aca52: src/main.rs
+
+/Users/atsuya/practice/rust/hello/target/debug/deps/hello-338f38284a7aca52.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/atsuya/practice/rust/hello/target/debug/hello: /Users/atsuya/practice/rust/hello/src/main.rs
