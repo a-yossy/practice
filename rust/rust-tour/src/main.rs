732ab5213c5399f23e0de555f546e32fdc0c2b1c
a-yossy <atsuya0930@yahoo.co.jp>
2024-08-12 04:50:18 +0900

    chapter4

--- rust/rust-tour/src/main.rs
+++ rust/rust-tour/src/main.rs
@@ -1,44 +1,17 @@
-#![allow(dead_code)]
-
-enum Species { Crab, Octopus, Fish, Clam }
-enum PoisonType { Acidic, Painful, Lethal }
-enum Size { Big, Small }
-enum Weapon {
-    Claw(i32, Size),
-    Poison(PoisonType),
-    None
-}
-
-struct SeaCreature {
-    species: Species,
-    name: String,
-    arms: i32,
-    legs: i32,
-    weapon: Weapon
-}
-
 fn main() {
-    let ferris = SeaCreature {
-        species: Species::Crab,
-        name: String::from("Ferris"),
-        arms: 2,
-        legs: 4,
-        weapon: Weapon::Claw(2, Size::Small)
-    };
+    let mut i32_vec = Vec::<i32>::new();
+    i32_vec.push(1);
+    i32_vec.push(2);
+    i32_vec.push(3);
 
-    match ferris.species {
-        Species::Crab => {
-            match ferris.weapon {
-                Weapon::Claw(num_class, size) => {
-                    let size_description = match size {
-                        Size::Big => "big",
-                        Size::Small => "small"
-                    };
-                    println!("ferris is a crab with {} {} claws", num_class, size_description)
-                },
-                _ => println!("ferris is a crab with some other weapon")
-            }
-        },
-        _ => println!("ferris is some other animal")
+    let mut float_vec = Vec::new();
+    float_vec.push(1.3);
+    float_vec.push(2.3);
+    float_vec.push(3.4);
+
+    let string_vec = vec![String::from("Hello"), String::from("World")];
+
+    for word in string_vec.iter() {
+        println!("{}", word)
     }
 }
