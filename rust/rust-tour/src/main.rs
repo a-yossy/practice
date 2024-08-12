b20a100cca483e822ee459b31276f0e9306c0396
a-yossy <atsuya0930@yahoo.co.jp>
2024-08-12 18:03:18 +0900

    chapter5

--- rust/rust-tour/src/main.rs
+++ rust/rust-tour/src/main.rs
@@ -1,17 +1,12 @@
+struct Foo<'a> {
+    i: &'a i32
+}
+
 fn main() {
-    let mut i32_vec = Vec::<i32>::new();
-    i32_vec.push(1);
-    i32_vec.push(2);
-    i32_vec.push(3);
+    let x = 42;
+    let foo = Foo {
+        i: &x
+    };
 
-    let mut float_vec = Vec::new();
-    float_vec.push(1.3);
-    float_vec.push(2.3);
-    float_vec.push(3.4);
-
-    let string_vec = vec![String::from("Hello"), String::from("World")];
-
-    for word in string_vec.iter() {
-        println!("{}", word)
-    }
+    println!("{}", foo.i);
 }
