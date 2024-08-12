9f57480f1b7921e7a259f891325a31718ff6ef2c
a-yossy <atsuya0930@yahoo.co.jp>
2024-08-12 22:57:07 +0900

    chapter6

--- rust/rust-tour/src/main.rs
+++ rust/rust-tour/src/main.rs
@@ -1,12 +1,7 @@
-struct Foo<'a> {
-    i: &'a i32
-}
-
-fn main() {
-    let x = 42;
-    let foo = Foo {
-        i: &x
-    };
-
-    println!("{}", foo.i);
+fn main() -> Result<(), std::num::ParseIntError> {
+    let a = 42;
+    let a_string = a.to_string();
+    let b = a_string.parse::<i32>()?;
+    println!("{} {}", a, b);
+    Ok(())
 }
