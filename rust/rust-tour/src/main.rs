2d081697e7bf8ae7e09892734cfd071aff495a60
a-yossy <atsuya0930@yahoo.co.jp>
2024-08-14 04:00:44 +0900

    chapter7

--- rust/rust-tour/src/main.rs
+++ rust/rust-tour/src/main.rs
@@ -1,7 +1,41 @@
-fn main() -> Result<(), std::num::ParseIntError> {
-    let a = 42;
-    let a_string = a.to_string();
-    let b = a_string.parse::<i32>()?;
-    println!("{} {}", a, b);
-    Ok(())
+struct SeaCreature {
+    pub name: String,
+    noise: String,
+}
+
+impl SeaCreature {
+    pub fn get_sound(&self) -> &str {
+        &self.noise
+    }
+}
+
+trait NoiseMaker {
+    fn make_noise(&self);
+}
+
+impl NoiseMaker for SeaCreature {
+    fn make_noise(&self) {
+        println!("{}", &self.get_sound());
+    }
+}
+
+struct Ocean {
+    animals: Vec<Box<dyn NoiseMaker>>,
+}
+
+fn main() {
+    let ferris = SeaCreature {
+        name: String::from("Ferris"),
+        noise: String::from("blub"),
+    };
+    let sarah = SeaCreature {
+        name: String::from("Sarah"),
+        noise: String::from("swish"),
+    };
+    let ocean = Ocean {
+        animals: vec![Box::new(ferris), Box::new(sarah)],
+    };
+    for a in ocean.animals.iter() {
+        a.make_noise();
+    }
 }
