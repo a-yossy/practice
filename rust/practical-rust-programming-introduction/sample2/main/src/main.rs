4a4275935a1010aa6ec46587e8b1bc82e0b3bcd9
a-yossy <atsuya0930@yahoo.co.jp>
2024-09-14 17:17:34 +0900

    chapter9

--- rust/practical-rust-programming-introduction/sample2/main/src/main.rs
+++ rust/practical-rust-programming-introduction/sample2/main/src/main.rs
@@ -2,4 +2,15 @@ use sub::hello;
 
 fn main() {
     hello();
+
+    let mut a = 0;
+    let mut b = 1;
+
+    #[allow(clippy::almost_swapped)]
+    {
+        a = b;
+        b = a;
+    }
+    dbg!(a);
+    dbg!(b);
 }
