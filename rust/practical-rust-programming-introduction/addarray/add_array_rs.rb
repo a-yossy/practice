007411b35af81795137aae3b8ab8efebc5b42f29
a-yossy <atsuya0930@yahoo.co.jp>
2024-09-17 18:38:35 +0900

    chapter11-2

---
+++ rust/practical-rust-programming-introduction/addarray/add_array_rs.rb
@@ -0,0 +1,21 @@
+require 'ffi'
+
+module AddArray
+  extend FFI::Library
+  ffi_lib './target/release/libaddarray.dylib'
+  attach_function :add_array, [:uint64, :uint64], :uint64
+end
+
+puts AddArray::add_array(ARGV[0].to_i, ARGV[1].to_i)
+
+def add_array_rb(n, x)
+  a = Array.new(n, 0)
+  x.times do
+    for i in 0..x-1
+      a[i] += 1
+    end
+  end
+  a.sum
+end
+
+puts add_array_rb(ARGV[0].to_i, ARGV[1].to_i)
