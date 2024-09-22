0116068d5d1f5cc0990889f387525ad5afaa2144
a-yossy <atsuya0930@yahoo.co.jp>
2024-09-22 23:11:49 +0900

    ...

--- rust/microsoft/rusty-journal/src/tasks.rs
+++ rust/microsoft/rusty-journal/src/tasks.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
 use std::io::{Error, Seek};
-use std::{fs::OpenOptions, io::ErrorKind, io::Result, io::SeekFrom, path::PathBuf};
+use std::{fmt, fs::OpenOptions, io::ErrorKind, io::Result, io::SeekFrom, path::PathBuf};
 
 use chrono::serde::ts_seconds;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -20,8 +20,15 @@ impl Task {
     }
 }
 
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
+}
+
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
@@ -47,6 +54,23 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
 
     file.set_len(0)?;
     serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
     Ok(())
 }
 
