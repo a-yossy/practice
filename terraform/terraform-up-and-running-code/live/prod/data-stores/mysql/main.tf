b51e96eaf3b10b7d4552b7428ff0d18783255646
a-yossy <atsuya0930@yahoo.co.jp>
2025-02-12 23:15:52 +0900

    ...

---
+++ terraform/terraform-up-and-running-code/live/prod/data-stores/mysql/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-2"
+  alias  = "primary"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "replica"
+}
+
+module "mysql_primary" {
+  source = "../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.primary
+  }
+
+  db_name     = "prod_db"
+  db_username = var.db_username
+  db_password = var.db_password
+
+  backup_retention_period = 1
+}
+
+module "mysql_replica" {
+  source = "../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.replica
+  }
+
+  replicate_source_db = module.mysql_primary.arn
+}
---
+++ terraform/terraform-up-and-running-code/modules/data-stores/mysql/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-yossy"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+
+  backup_retention_period = var.backup_retention_period
+  replicate_source_db = var.replicate_source_db
+
+  engine = var.replicate_source_db == null ? "mysql" : null
+  db_name = var.replicate_source_db == null ? var.db_name : null
+  username = var.replicate_source_db == null ? var.db_username : null
+  password = var.replicate_source_db == null ? var.db_password : null
+}
