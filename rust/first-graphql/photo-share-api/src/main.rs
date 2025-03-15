8ca799abac8560aeb4bef54a1e53783f95304566
a-yossy <atsuya0930@yahoo.co.jp>
2025-03-15 17:49:18 +0900

    ...

--- rust/first-graphql/photo-share-api/src/main.rs
+++ rust/first-graphql/photo-share-api/src/main.rs
@@ -1,4 +1,6 @@
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
+use std::sync::Mutex;
+
+use async_graphql::{http::GraphiQLSource, EmptySubscription, Object, Schema, SimpleObject};
 use async_graphql_axum::GraphQL;
 use axum::{
     response::{self, IntoResponse},
@@ -7,12 +9,45 @@ use axum::{
 };
 use tokio::net::TcpListener;
 
+#[derive(SimpleObject, Clone)]
+struct Photo {
+    id: String,
+    name: String,
+    description: Option<String>,
+}
+
+static PHOTOS: Mutex<Vec<Photo>> = Mutex::new(Vec::new());
+static ID: Mutex<usize> = Mutex::new(0);
+
 struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn total_photos(&self) -> i32 {
-        42
+    async fn total_photos(&self) -> usize {
+        let photos = PHOTOS.lock().unwrap();
+        photos.len()
+    }
+
+    async fn all_photos(&self) -> Vec<Photo> {
+        let photos = PHOTOS.lock().unwrap();
+        photos.to_vec()
+    }
+}
+
+struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+    async fn post_photo(&self, name: String, description: Option<String>) -> Photo {
+        let mut photos = PHOTOS.lock().unwrap();
+        let mut id = ID.lock().unwrap();
+        *id += 1;
+        let new_photo = Photo {
+            id: id.to_string(), name, description
+        };
+        photos.push(new_photo.clone());
+
+        new_photo
     }
 }
 
@@ -22,7 +57,7 @@ async fn grashiql() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish();
     let app = Router::new().route("/", get(grashiql).post_service(GraphQL::new(schema)));
     axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
         .await
