f17772b827c93f4ed2afb3a2abf1b58369bd9f81
a-yossy <atsuya0930@yahoo.co.jp>
2025-03-21 18:50:53 +0900

    ...

---
+++ rust/first-graphql/photo-share-client/src/Users.tsx
@@ -0,0 +1,70 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+const ROOT_QUERY = gql`
+  query allUsers {
+    totalUsers
+    allUsers {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
+const UserList = ({ count, users, refetchUsers }) => {
+  const [addFakeUsers] = useMutation(ADD_FAKE_USERS_MUTATION, {
+    refetchQueries: [{ query: ROOT_QUERY }],
+    variables: { count: 1 },
+  });
+
+  return (
+    <div>
+      <p>{count} Users</p>
+      <button onClick={() => refetchUsers()}>Refetch Users</button>
+      <button onClick={addFakeUsers}>Add Fake Users</button>
+      <ul>
+        {users.map((user) => (
+          <UserListItem
+            key={user.githubLogin}
+            name={user.name}
+            avatar={user.avatar}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const UserListItem = ({ name, avatar }) => (
+  <li>
+    <img src={avatar} width={48} height={48} alt="" />
+    {name}
+  </li>
+);
+
+export const Users = () => {
+  const { loading, error, data, refetch } = useQuery(ROOT_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <UserList
+        count={data.totalUsers}
+        users={data.allUsers}
+        refetchUsers={refetch}
+      />
+    </>
+  );
+};
--- rust/first-graphql/photo-share-client/src/main.tsx
+++ rust/first-graphql/photo-share-client/src/main.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import './main.css';
-import reactLogo from './assets/react.svg';
-import FarmLogo from './assets/logo.png';
+import "./main.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import { Users } from "./Users";
 export function Main() {
-  const [count, setCount] = useState(0);
+  const client = new ApolloClient({
+    uri: "http://localhost:8000",
+    cache: new InMemoryCache(),
+  });
+
+  console.log({ cache: client.extract() });
+  const query = gql`
+    {
+      totalUsers
+      totalPhotos
+    }
+  `;
+  client
+    .query({ query })
+    .then(() => console.log({ cache: client.extract() }))
+    .then(console.error);
 
   return (
-    <>
-      <div>
-        <a href="https://farmfe.org/" target="_blank">
-          <img src={FarmLogo} className="logo" alt="Farm logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Farm + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/main.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Farm and React logos to learn more
-      </p>
-    </>
+    <ApolloProvider client={client}>
+      <Users />
+    </ApolloProvider>
   );
 }
