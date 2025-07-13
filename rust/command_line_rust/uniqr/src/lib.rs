2490401463e5b7808ec7266d5d8fe671feb0c1bd
a-yossy <atsuya0930@yahoo.co.jp>
2025-07-13 21:01:55 +0900

    chapter6

--- rust/command_line_rust/uniqr/src/lib.rs
+++ rust/command_line_rust/uniqr/src/lib.rs
@@ -1,4 +1,9 @@
-use anyhow::Result;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
+};
+
+use anyhow::{Result, anyhow};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -18,6 +23,49 @@ pub struct Args {
 }
 
 pub fn run(args: Args) -> Result<()> {
-    println!("{:?}", args);
+    let mut file = open(&args.in_file).map_err(|e| anyhow!("{}: {e}", args.in_file))?;
+    let mut out_file: Box<dyn Write> = match &args.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+    let mut print = |count: u64, text: &str| -> Result<()> {
+        if count > 0 {
+            if args.count {
+                write!(out_file, "{count:>4} {text}")?;
+            } else {
+                write!(out_file, "{text}")?;
+            }
+        }
+        Ok(())
+    };
+
+    let mut line = String::new();
+    let mut previous = String::new();
+    let mut count = 0;
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous)?;
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous)?;
+
     Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
