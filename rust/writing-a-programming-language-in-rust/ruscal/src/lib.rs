ee3c2076b97f747349cb6b35d7a5223fe3f3d77f
a-yossy <atsuya0930@yahoo.co.jp>
2026-01-02 11:11:31 +0900

    ...

---
+++ rust/writing-a-programming-language-in-rust/ruscal/src/lib.rs
@@ -0,0 +1,106 @@
+use std::sync::atomic::AtomicBool;
+
+pub enum RunMode {
+    None,
+    TypeCheck,
+    Compile,
+    Run(String),
+    CompileAndRun,
+}
+
+pub struct Args {
+    pub run_mode: RunMode,
+    pub source: Option<String>,
+    pub output: String,
+    pub disasm: bool,
+    pub debug_output: bool,
+}
+
+impl Args {
+    pub fn new() -> Self {
+        Self {
+            run_mode: RunMode::CompileAndRun,
+            source: None,
+            output: "".to_string(),
+            disasm: false,
+            debug_output: false,
+        }
+    }
+}
+
+impl Default for Args {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub static DEBUG: AtomicBool = AtomicBool::new(false);
+
+pub fn parse_args() -> Option<Args> {
+    let mut run_mode = RunMode::None;
+    let mut source = None;
+    let mut output = None;
+    let mut disasm = false;
+    let mut show_help = false;
+    let mut args_is_empty = true;
+    let mut debug_output = false;
+
+    let mut args = std::env::args();
+    let _exe = args.next();
+    let mut next_arg = args.next();
+    while let Some(arg) = next_arg {
+        match &arg as &str {
+            "-h" => show_help = true,
+            "-c" => run_mode = RunMode::Compile,
+            "-o" => output = args.next(),
+            "-r" => {
+                let bytecode = args.next().unwrap_or_else(|| "bytecode.bin".to_string());
+                run_mode = RunMode::Run(bytecode)
+            }
+            "-R" => run_mode = RunMode::CompileAndRun,
+            "-d" => disasm = true,
+            "-D" => {
+                DEBUG.store(true, std::sync::atomic::Ordering::Relaxed);
+                debug_output = true;
+            }
+            _ => {
+                if source.is_none() {
+                    source = Some(arg)
+                } else {
+                    println!("More than 1 file name is specified");
+                    return None;
+                }
+            }
+        }
+        args_is_empty = false;
+        next_arg = args.next();
+    }
+
+    if show_help || args_is_empty {
+        return None;
+    }
+
+    Some(Args {
+        run_mode,
+        source,
+        output: output.unwrap_or_else(|| "bytecode.bin".to_string()),
+        disasm,
+        debug_output,
+    })
+}
+
+#[macro_export]
+macro_rules! dprintln {
+    ($fmt:literal) => {
+        #[cfg(not(target_arch = "wasm32"))]
+        if ::ruscal::DEBUG.load(std::sync::atomic::Ordering::Relaxed) {
+            println!($fmt);
+        }
+    };
+    ($fmt:literal, $($args:expr),*) => {
+        #[cfg(not(target_arch = "wasm32"))]
+        if ::ruscal::DEBUG.load(std::sync::atomic::Ordering::Relaxed) {
+            println!($fmt, $($args),*);
+        }
+    }
+}
